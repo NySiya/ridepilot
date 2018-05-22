a96a54a960b9655c33070cfc3991eb04e4ffa52a
Siya Ny <siya.nyinfo@gmail.com>
2018-05-22 19:04:07 +0700

    add application and database

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
-set :production
-
-set :app_name, 'ridepilot'
-
-server '188.166.227.111', user: 'deployer', roles: %w{app web db}
+# set :production
+# 
+# set :app_name, 'ridepilot'
+# 
+# server '188.166.227.111', user: 'deployer', roles: %w{app web db}
