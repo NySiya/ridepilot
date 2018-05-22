85156e589bc39811457666ee5b07dc4dc1f52c05
Siya Ny <siya.nyinfo@gmail.com>
2018-05-22 22:04:51 +0700

    fix missing deleted_at column

--- db/migrate/20110304000356_defaults.rb
+++ db/migrate/20110304000356_defaults.rb
@@ -2,6 +2,7 @@ class Defaults < ActiveRecord::Migration[5.1]
   def self.up
     change_column_default(:trips, :guest_count, 0)
     change_column_default(:trips, :attendant_count, 0)
+    change_column_default(:trips, :deleted_at, 0)
     change_column_default(:trips, :group_size, 0)
     change_column_default(:trips, :donation, 0)
   end
@@ -9,6 +10,7 @@ class Defaults < ActiveRecord::Migration[5.1]
   def self.down
     change_column_default(:trips, :guest_count, nil)
     change_column_default(:trips, :attendant_count, nil)
+    change_column_default(:trips, :deleted_at, nil)
     change_column_default(:trips, :group_size, nil)
     change_column_default(:trips, :donation, nil)
   end
